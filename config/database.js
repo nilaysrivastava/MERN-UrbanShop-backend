270bcf5b4976683e1a3483b237f275ef01e5c6b9
nilaysrivastava <nilay2103@gmail.com>
2024-05-25 17:55:35 +0530

    initial commit

---
+++ config/database.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+// const connectDb = () => {
+//     console.log("a");
+//   mongoose.connect(process.env.DB_URI).then((data) => {
+//       console.log(`Mongodb connected with server: ${data.connection.host}`);
+//     }).catch((err)=>{
+//         console.log(err);
+//     })
+//     console.log("b");
+// };
+
+// const connectDb = () => {
+//     mongoose.createConnection(process.env.DB_URI).asPromise();
+// };
+connectDb().catch(err => console.log(err));
+
+async function connectDb() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/UrbanShop');
+  console.log('db connected')
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+module.exports = connectDb;
